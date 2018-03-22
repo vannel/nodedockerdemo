8b8b004e5bf1e966162be3efa89725a99fbeaad3
vannel_liu@163.com <rukawa8317_gh>
2018-03-22 11:32:25 +0800

    fixed: include leancloud

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+
+var AV = require('leanengine');
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID,
+  appKey: process.env.LEANCLOUD_APP_KEY,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+});
+
+
+
 //引入http模块
 var http = require("http");
 //设置主机名
 var hostName = '127.0.0.1';
 //设置端口
-var port = 80;
+var port = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
+
 //创建服务
 var server = http.createServer(function(req,res){
     res.setHeader('Content-Type','text/plain');
